1c26489418862dc3c147b05d91449d6819ecce81
GiorgioDordo <djole6789@gmail.com>
2024-09-16 20:20:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppContent from './components/AppContent.vue'
+import AppNavbar from './components/AppNavbar.vue'
+import AppInfo from './components/AppInfo.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components:{
+    AppHeader,
+    AppContent,
+    AppNavbar,
+    AppInfo,
+    AppFooter
+  }
+}
+</script>
+
+<template>
+<AppHeader />
+<AppContent />
+<AppNavbar />
+<AppInfo />
+<AppFooter />
+</template>
+
+<style lang="scss">
+@use "../node_modules/bootstrap/scss/bootstrap.scss" as *;
+@use "./styles/general.scss" as *;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<template>
+<header >
+    <ul class="d-flex gap-3 list-unstyled">
+        <li><a href="">header</a></li>
+        <li><a href="">header</a></li>
+        <li><a href="">header</a></li>
+        <li><a href="">header</a></li>
+        <li><a href="">header</a></li>
+        <li><a href="">header</a></li>
+        <li><a href="">header</a></li>
+        <li><a href="">header</a></li>
+        <li><a href="">header</a></li>
+        <li><a href="">header</a></li>
+        <li><a href="">header</a></li>
+        <li><a href="">header</a></li>  
+    </ul>
+</header>
+</template>
+
+<style lang="scss">
+
+</style>
