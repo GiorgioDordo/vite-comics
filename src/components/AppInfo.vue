6ed3c5c3a9b2389dcb2caad006d8c09ebaaef2ca
GiorgioDordo <djole6789@gmail.com>
2024-09-17 19:56:36 +0200

    added info to the page

--- src/components/AppInfo.vue
+++ src/components/AppInfo.vue
@@ -1,11 +1,50 @@
 <script>
+export default {
+    name: 'AppInfo',
+    data () {
+        return {
+            titleList: [
+                {name: 'DC COMICS',
+                 list: ["info", "info"],   
+                },
+                {name: 'SHOP',
+                list: ["info", "info"],  
+                },
+                {name: 'DC',
+                list: ["info", "info"],  
+                },
+                {name: 'SITES',
+                list: ["info", "info"],
+                },
+            ],
 
+        }
+    }
+}
 </script>
 
 <template>
-<header><h1>Header</h1></header>
+    <section id="info"> 
+        <div class="container">
+            <div class="column flex-wrap">
+                <div v-for="(item, index) in titleList" :key="index" class="col">
+                    <h5>{{ item.name }}</h5>
+                    <ul class="list-unstyled">
+                        <li v-for="(listItem, index) in item.list" :key="index">
+                            <a href="">{{ listItem }}</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
+#info {
+    color: white;
+    background-image: url(../assets/img/footer-bg.jpg);
+    height: 500px;
+}
 
 </style>
