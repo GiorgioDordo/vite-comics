851fa4a6a677bc12835b642d5a0c86a9bc806a89
GiorgioDordo <djole6789@gmail.com>
2024-09-17 17:03:50 +0200

    header done

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -3,9 +3,19 @@
 </script>
 
 <template>
-<header><h1>Header</h1></header>
+<section id="content">
+    <div class="content-container container text-white d-flex align-items-center">
+        <h1>-->content goes here<--</h1>
+    </div>
+</section>
 </template>
 
 <style lang="scss" scoped>
-
+#content {
+    background-color: black;
+    .content-container {
+    height: 100px;
+    background-color: black;
+}
+}
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,54 @@
 <script>
+export default {
+data () {
+    return {
+        name: 'AppHeader',
+        linkList:[
+            {name: 'Characters', active: false},
+            {name: 'Comics', active: false},
+            {name: 'Movies', active: true},
+            {name: 'Tv', active: false},
+            {name: 'Games', active: false},
+            {name: 'Collectibles', active: false},
+            {name: 'Videos', active: false},
+            {name: 'Fans', active: false},
+            {name: 'News', active: false},
+            {name: 'Shop', active: false}
+        ]
+    }
+   },
+   methods: {
+    addActiveIndex(index) {
+        this.linkList.forEach((item, i) => {
+            item.active = i === index;
+        })
+    }
+   }
+ }
 </script>
 
 <template>
-<header >
-    <ul class="d-flex gap-3 list-unstyled">
-        <li><a href="">header</a></li>
-    </ul>
+<header>
+    <div class="nav-container container d-flex justify-content-between align-items-center">
+        <div>
+            <img id="dc-logo" src="../assets/img/dc-logo.png" alt="dc logo">
+        </div>
+        
+        <nav>
+            <ul class="list-unstyled d-flex gap-3">
+                <li v-for="(item, index) in linkList" :key="index" :class="{active: item.active}" @click="addActiveIndex(index)" >{{ item.name }}</li>
+            </ul>
+        </nav>
+    </div>
 </header>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.nav-container {
+    height: 100px;
 
+    #dc-logo {
+        height: 80px;
+    }
+}
 </style>
