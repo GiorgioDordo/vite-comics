54f920561cee39c546510b34b480e0c147f4fa33
GiorgioDordo <djole6789@gmail.com>
2024-09-17 16:02:46 +0200

    added images

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,17 +5,6 @@
 <header >
     <ul class="d-flex gap-3 list-unstyled">
         <li><a href="">header</a></li>
-        <li><a href="">header</a></li>
-        <li><a href="">header</a></li>
-        <li><a href="">header</a></li>
-        <li><a href="">header</a></li>
-        <li><a href="">header</a></li>
-        <li><a href="">header</a></li>
-        <li><a href="">header</a></li>
-        <li><a href="">header</a></li>
-        <li><a href="">header</a></li>
-        <li><a href="">header</a></li>
-        <li><a href="">header</a></li>  
     </ul>
 </header>
 </template>
